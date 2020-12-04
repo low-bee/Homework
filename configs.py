8bd06e4e8c41a8653c25690ac8a2bf67ec8e2449
low-bee <57255667+low-bee@users.noreply.github.com>
2020-12-04 18:49:42 +0800

    所有文件 (#1)
    Co-authored-by: xiaolong <woaini520523@outlook.com>

---
+++ configs.py
@@ -0,0 +1,8 @@
+HOSTNAME = 'localhost'
+DATABASE = 'alchemy'
+USERNAME = 'root'
+PASSWORD = 'chuanzhi'
+DBNAME = "lagou"
+DB_URI = 'mysql://{}:{}@{}/{}'.format(
+    USERNAME, PASSWORD, HOSTNAME, DATABASE
+)
